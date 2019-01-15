c144794f50eec83471aa82272e33493351905c01
Sangdon Park <pravusid@gmail.com>
2019-01-15 23:11:58 +0900

    add: validation

--- src/api/home.controller.ts
+++ src/api/home.controller.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { getRepository } from 'typeorm';
 import { Post } from '../domain/post';
 
@@ -13,13 +13,21 @@ class HomeController {
   async getPost(req: Request, resp: Response) {
     const id = req.params.id;
     const post = await getRepository(Post).findOne(id);
-    resp.status(200).send(post);
+    (post ? resp.status(200) : resp.status(404)).json(post);
   }
 
-  async savePost(req: Request, resp: Response) {
-    const post = req.body;
-    await getRepository(Post).save(post);
-    resp.status(201).send(post);
+  async savePost(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const { title, author, content } = req.body;
+      const newPost = new Post(title, author, content);
+      if (!newPost.isValid()) {
+        throw new Error('invalid');
+      }
+      const post = await getRepository(Post).save(newPost);
+      resp.status(201).json(post.id);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- src/domain/post.ts
+++ src/domain/post.ts
@@ -13,4 +13,14 @@ export class Post {
 
   @Column()
   content: string;
+
+  constructor(title: string, author: string, content: string) {
+    this.title = title;
+    this.author = author;
+    this.content = author;
+  }
+
+  isValid(): boolean {
+    return this.title.length !== 0 && this.author.length !== 0;
+  }
 }

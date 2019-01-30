c2d5771effaecb1c9069c35a619cabb7bd97c990
Sangdon Park <pravusid@gmail.com>
2019-01-30 23:40:39 +0900

    fix: path of entities for compiled js files

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,17 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
 import { CustomNamingStrategy } from './custom.naming.strategy';
 
+const entities =
+  process.env.NODE_ENV === 'production'
+    ? 'dist/domain/**/*.js'
+    : 'src/domain/**/*.ts';
+
 export const connectToDatabase = async () => {
   const option = await getConnectionOptions();
-  return await createConnection(Object.assign(option, {
-    namingStrategy: new CustomNamingStrategy(),
-  }));
+  return await createConnection(
+    Object.assign(option, {
+      namingStrategy: new CustomNamingStrategy(),
+      entities: [entities],
+    }),
+  );
 };

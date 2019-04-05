1c24dbe3a213754a2206e29c6c9779d00e8a77f5
Sangdon Park <pravusid@gmail.com>
2019-04-05 21:52:08 +0900

    fix: source of ormconfig file in various env

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,12 +2,13 @@ import { createConnection, getConnectionOptions, ConnectionOptionsReader } from 
 import { CustomNamingStrategy } from './custom.naming.strategy';
 
 export const connectToDatabase = async () => {
-  const connectionOptions = await (process.env.NODE_ENV === 'production'
-    ? getConnectionOptions()
-    : new ConnectionOptionsReader({ configName: 'ormconfig.dev' }).get('default'));
+  const env = process.env.NODE_ENV;
+  const configName = env === 'production' ? 'ormconfig' : `ormconfig.${env}`;
+  const connectionOptions = new ConnectionOptionsReader({ configName }).all();
+  const [connectionOption] = await connectionOptions;
 
-  return await createConnection(
-    Object.assign(connectionOptions, {
+  return createConnection(
+    Object.assign(connectionOption, {
       namingStrategy: new CustomNamingStrategy(),
     }),
   );

7590600df4a72a5c264ad2f68fab34d127778f7c
Sangdon Park <pravusid@gmail.com>
2019-08-31 21:06:12 +0900

    update: debugging environment

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,10 @@
 import { createConnection, ConnectionOptionsReader } from 'typeorm';
 import { CustomNamingStrategy } from './custom.naming.strategy';
 
-export const connectToDatabase = async () => {
-  const env = process.env.NODE_ENV;
-  const configName = env === 'production' ? 'ormconfig' : `ormconfig.${env}`;
+export const connectToDatabase = async (env?: string) => {
+  const entityEnv = process.env.ENTITY_ENV;
+  const configName = env || entityEnv ? `ormconfig.${env || entityEnv}` : 'ormconfig';
+
   const connectionOptions = new ConnectionOptionsReader({ configName }).all();
   const [connectionOption] = await connectionOptions;
 

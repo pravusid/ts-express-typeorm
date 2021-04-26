823e61bff2d7807cb196710ee3fe88987acb66d4
Sangdon Park <pravusid@gmail.com>
2021-04-27 00:16:52 +0900

    deps: bump version

--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -2,7 +2,8 @@ import { validate } from 'class-validator';
 import { CustomValidationError } from '../domain/error/validation.error';
 
 export const validation = async <T>(arg: T): Promise<T> => {
-  const errors = await validate(arg);
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const errors = await validate((arg as unknown) as object);
 
   if (errors.length > 0) {
     throw new CustomValidationError(errors);

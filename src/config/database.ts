36922f7561974765e8a59110d50153d70fc5bc1a
Sangdon Park <pravusid@gmail.com>
2021-04-28 02:30:13 +0900

    refactor: removed unused code

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,10 @@
 import { join } from 'path';
-import { ConnectionOptionsReader, createConnection, getConnection, Connection } from 'typeorm';
+import { Connection, ConnectionOptionsReader, createConnection } from 'typeorm';
 import { CustomNamingStrategy } from './custom.naming.strategy';
 
 export const connectToDatabase = async (): Promise<Connection> => {
-  const connectionOptions = new ConnectionOptionsReader().all();
-  const [connectionOption] = await connectionOptions;
+  const reader = new ConnectionOptionsReader();
+  const [connectionOption] = await reader.all();
 
   return createConnection(
     Object.assign(connectionOption, {
@@ -13,5 +13,3 @@ export const connectToDatabase = async (): Promise<Connection> => {
     }),
   );
 };
-
-export const disconnectFromDatabase = (): Promise<void> => getConnection().close();

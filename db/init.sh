ac3d9462314e7092b8e9380e5f133795ecc4eea9
Sangdon Park <24415130+pravusid@users.noreply.github.com>
2023-09-14 00:12:46 +0900

    db: changed script

--- db/init.sh
+++ db/init.sh
@@ -5,14 +5,17 @@ cd $current_dir
 
 DEV_CONTAINER_NAME="tsexpress"
 
-docker run \
-  --detach \
-  --env MYSQL_ROOT_PASSWORD=1234 \
-  --env MYSQL_USER=tsexpress \
-  --env MYSQL_PASSWORD=1234 \
-  --env MYSQL_DATABASE=tsexpress \
-  --name tsexpress \
-  --publish 3306:3306 \
+docker \
+  run \
+  -d \
+  --name $DEV_CONTAINER_NAME \
+  -p 3306:3306 \
+  -v "$(pwd)"/scripts/:/docker-entrypoint-initdb.d/ \
+  -e MYSQL_ROOT_PASSWORD=1234 \
+  -e MYSQL_USER=tsexpress \
+  -e MYSQL_PASSWORD=1234 \
+  -e MYSQL_DATABASE=tsexpress \
+  -e LANG=C.UTF-8 \
   mariadb:latest \
   --character-set-server=utf8mb4 \
   --collation-server=utf8mb4_unicode_ci
